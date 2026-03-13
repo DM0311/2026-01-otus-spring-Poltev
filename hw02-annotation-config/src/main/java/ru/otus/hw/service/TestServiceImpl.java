00a116c545f5117e40c8391273c9ae413e68b826
poltev_dv <poltev_dv@nlmk.com>
2026-03-13 12:56:49 +0300

    hw02: completed

---
+++ hw02-annotation-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -0,0 +1,44 @@
+package ru.otus.hw.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.domain.Student;
+import ru.otus.hw.domain.TestResult;
+
+@Service
+@RequiredArgsConstructor
+public class TestServiceImpl implements TestService {
+
+    private final IOService ioService;
+
+    private final QuestionDao questionDao;
+
+    @Override
+    public TestResult executeTestFor(Student student) {
+        ioService.printLine("");
+        ioService.printFormattedLine("Please answer the questions below%n");
+        var questions = questionDao.findAll();
+        var testResult = new TestResult(student);
+
+        for (var question : questions) {
+            var output = toFormattedString(question);
+            ioService.printLine(output);
+            var answerNum = ioService.readIntForRange(0, question.answers().size() - 1, "Incorrect number");
+            var isAnswerValid = question.answers().get(answerNum).isCorrect();
+            testResult.applyAnswer(question, isAnswerValid);
+        }
+        return testResult;
+    }
+
+    private String toFormattedString(Question question) {
+        StringBuilder builder = new StringBuilder(question.text());
+        builder.append(System.lineSeparator());
+        for (int i = 0; i < question.answers().size(); i++) {
+            builder.append(String.format("%d. %s%n", i, question.answers().get(i).text()));
+        }
+        return builder.toString();
+    }
+
+}
