7d3f7e65303429ab0ca1a9a05fba9c6dbed249ef
DM0311 <karapuzoff90@mail.ru>
2026-04-06 14:42:29 +0300

    hw03-spring-boot: finished (#3)
    * hw03-spring-boot: finished
    
    * hw03-spring-boot: comments fixed
    
    * hw03-spring-boot: tests fixed
    
    ---------
    
    Co-authored-by: poltev_dv <poltev_dv@nlmk.com>

---
+++ hw03-spring-boot/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -0,0 +1,50 @@
+package ru.otus.hw.dao;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class CsvQuestionDao implements QuestionDao {
+    private final TestFileNameProvider fileNameProvider;
+
+    @Override
+    public List<Question> findAll() {
+        List<Question> questions = new ArrayList<>();
+        String fileName = fileNameProvider.getTestFileName();
+        ClassLoader classLoader = getClass().getClassLoader();
+        try (InputStream is = classLoader.getResourceAsStream(fileName)) {
+            if (is == null) {
+                throw new QuestionReadException("File not found in classpath");
+            }
+            Reader reader = new InputStreamReader(is, StandardCharsets.UTF_8);
+            CsvToBean<QuestionDto> csvToBean = new CsvToBeanBuilder<QuestionDto>(reader)
+                    .withType(QuestionDto.class)
+                    .withSeparator(';')
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .withSkipLines(1)
+                    .build();
+            var result = csvToBean.parse();
+            for (QuestionDto dto : result) {
+                questions.add(dto.toDomainObject());
+            }
+            return questions;
+        } catch (IOException e) {
+            throw new QuestionReadException("File not found in classpath", e);
+        }
+    }
+}
