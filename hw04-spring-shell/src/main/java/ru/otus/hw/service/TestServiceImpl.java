384d2214105398596e4abceda01c488d218a4e4c
DM0311 <karapuzoff90@mail.ru>
2026-04-15 11:32:57 +0300

    hw04-spring-shell: finished (#4)
    Co-authored-by: poltev_dv <poltev_dv@nlmk.com>

---
+++ hw04-spring-shell/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -0,0 +1,52 @@
+package ru.otus.hw.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.domain.Student;
+import ru.otus.hw.domain.TestResult;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TestServiceImpl implements TestService {
+
+    private final LocalizedIOService ioService;
+
+    private final QuestionDao questionDao;
+
+    @Override
+    public TestResult executeTestFor(Student student) {
+        ioService.printLine("");
+        ioService.printLineLocalized("TestService.answer.the.questions");
+        ioService.printLine("");
+
+        var questions = questionDao.findAll();
+        return executeSurvey(questions, student);
+    }
+
+    private TestResult executeSurvey(List<Question> questions, Student student) {
+        var testResult = new TestResult(student);
+        for (var question : questions) {
+            var output = toFormattedString(question);
+            ioService.printLine(output);
+            var errorMsg = ioService.getMessage("Testservice.input.incorrect.number");
+            var answerNum = ioService.readIntForRange(0, question.answers().size() - 1, errorMsg);
+            var isAnswerValid = question.answers().get(answerNum).isCorrect();
+            testResult.applyAnswer(question, isAnswerValid);
+        }
+        return testResult;
+    }
+
+    private String toFormattedString(Question question) {
+        StringBuilder builder = new StringBuilder(question.text());
+        builder.append(System.lineSeparator());
+        for (int i = 0; i < question.answers().size(); i++) {
+            builder.append(String.format("%d. %s%n", i, question.answers().get(i).text()));
+        }
+        return builder.toString();
+    }
+
+}
