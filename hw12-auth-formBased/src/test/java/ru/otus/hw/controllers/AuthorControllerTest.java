e555b3bf7d5cc0e453aa646b53381b9dd6f0bee6
poltev_dv <poltev_dv@nlmk.com>
2026-06-29 13:31:49 +0300

    hw12-auth-formBased

--- hw12-auth-formBased/src/test/java/ru/otus/hw/controllers/AuthorControllerTest.java
+++ hw12-auth-formBased/src/test/java/ru/otus/hw/controllers/AuthorControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AuthorController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class AuthorControllerTest {
 
     @Autowired
--- hw12-auth-formBased/src/test/java/ru/otus/hw/controllers/CommentControllerTest.java
+++ hw12-auth-formBased/src/test/java/ru/otus/hw/controllers/CommentControllerTest.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.otus.hw.dto.CommentDto;
@@ -19,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CommentController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class CommentControllerTest {
     @Autowired
     private MockMvc mvc;
@@ -31,6 +34,7 @@ public class CommentControllerTest {
 
     @DisplayName("Должен вернуть все комментарии к книге")
     @Test
+
     void shouldReturnAllAuthors() throws Exception {
         List<CommentDto> comments = List.of(new CommentDto(1L,"Comments_1"));
         given(commentService.findAllByBookId(1L)).willReturn(comments);
